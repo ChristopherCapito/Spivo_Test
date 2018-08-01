502116891da12fd02ee3630ad3c67e8d173d42f0
Unknown <ccapito94@gmail.com>
2018-08-01 21:02:09 +0200

    V0.2
    - Fixed Antialiasing
    - Fixed PixelRatio (Blur Zoom)
    - Added Effectlights
    - Changed to Trackballcontrol (Gimballock disabled)
    - Fixed Shadows

--- js/main.js
+++ js/main.js
@@ -7,27 +7,46 @@ camera = new THREE.PerspectiveCamera(30, 1, 1, 100000);
 camera.position.x = 0;
 camera.position.y = 0;
 camera.position.z = 50;
+camera.useQuaternion = true;
+
 camera.lookAt({
     x: 0,
     y: 0,
     z: 0
 });
 
+
+
 //Camera Controls
 var controls = new THREE.OrbitControls(camera, container);
 controls.enableZoom = false;
 controls.enableKeys = false;
 controls.enablePan = false;
+controls.enableRotate = false;
 controls.enableDamping = true;
 controls.dampingFactor = 0.1;
 controls.rotateSpeed = 0.05;
 controls.autoRotateSpeed = 0.5;
 controls.autoRotate = true;
 
+
+//Trackballcontrols
+var tcontrols = new THREE.TrackballControls(camera, container);
+tcontrols.noPan=true;
+tcontrols.noZoom=true;
+tcontrols.dynamicDampingFactor=0.05;
+tcontrols.rotateSpeed=1;
+tcontrols.enableKeys = false;
+
+
+
+
+
 // Light Color Config
 var directionalLightColor = 0xEAF2FF;
-var ambientLightColor = 0x666666;
-var pointLightColor = 0xC9CEFF;
+var backlightcolor = 0x007CBA;
+var filllightcolor = 0xAF1A73;
+var ambientLightColor = 0xC9E2FE;
 
 init();
 animate();
@@ -47,18 +66,20 @@ function init() {
 
     //Initialize Scene
     scene = new THREE.Scene();
+    scene.add(camera);
+    
 
 
     //#region Lighting
     /*All lights are set here. This includes all shadow properties */
 
     //Add ambient light
-    scene.add(new THREE.AmbientLight(ambientLightColor, 2));
+    scene.add(new THREE.AmbientLight(ambientLightColor, 0.8));
 
     //Set up Directional Light
     var light;
-    light = new THREE.DirectionalLight(directionalLightColor, 0.5);
-    light.position.set(300, 400, 50);
+    light = new THREE.DirectionalLight(directionalLightColor, 1);
+    light.position.set(300, -200, 0);
     light.position.multiplyScalar(1.3);
 
     //Set up Directional Light Shadow Properties
@@ -66,22 +87,32 @@ function init() {
     light.shadow.mapSize.width = 1024;
     light.shadow.mapSize.height = 1024;
 
-    var d = 20;
+    var d = 10;
 
     light.shadow.camera.left = -d;
     light.shadow.camera.right = d;
     light.shadow.camera.top = d;
     light.shadow.camera.bottom = -d;
-    light.shadow.camera.far = 10000;
+    light.shadow.camera.far = 1000;
+    light.shadowDarkness = 10;
 
     //Add Directional light to scene
-    scene.add(light);
+    camera.add(light);
 
     //Add a backlight
-    var backlight;
-    backlight = new THREE.PointLight(pointLightColor, 0.2);
-    backlight.position.set(-200, 0, 50);
-    scene.add(backlight);
+    var backlight;    
+    backlight = new THREE.DirectionalLight(backlightcolor, 0.5);
+    backlight.position.set(-200, 400, 0);
+    backlight.castShadow=true;
+    camera.add(backlight);
+    
+
+    //Add a filllight
+    var filllight;
+    filllight = new THREE.DirectionalLight(filllightcolor, 0.5);
+    filllight.position.set(-450, -450, 0);
+    filllight.castShadow=true;
+    camera.add(filllight);
     //#endregion
 
     //#region Object Loading
@@ -119,6 +150,7 @@ function init() {
                 }
             });
             object.castShadow = true;
+            object.receiveShadow = true;
             scene.add(object);
         });
         //#endregion
@@ -132,12 +164,16 @@ function init() {
         antialias: true
     });
 
+
+
+
     //Resizer Tool only kinda works, does not limit to the div
     //var winResize = new THREEx.WindowResize(renderer,camera);
     //winResize.trigger();
 
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.typeype = THREE.PCFSoftShadowMap;
+   
 
     //Not the optimal solution but eh
     resizeCanvasToDisplaySize(true);
@@ -152,19 +188,24 @@ function resizeCanvasToDisplaySize(force) {
         // you must pass false here or three.js sadly fights the browser
         renderer.setSize(width, height, false);
         camera.aspect = width / height;
+        
         camera.updateProjectionMatrix();
         // set render target sizes here
+       
     }
 }
 
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
+    tcontrols.update();      
     render();
 }
 
 function render() {
     resizeCanvasToDisplaySize();
+    tcontrols.handleResize();  
+    renderer.setPixelRatio(3); 
     camera.lookAt(scene.position);
     renderer.render(scene, camera);
 }
